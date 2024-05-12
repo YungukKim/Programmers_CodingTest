2a10e02ba81408a8cb46269bb60613768569dff2
Yunguk Kim / 김윤국 <160746652+YungukKim@users.noreply.github.com>
2024-05-13 01:36:26 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.02 ms, Memory: 82 MB -BaekjoonHub

---
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        double num3;
+        double num4;
+        
+        num3 = num1;
+        num4 = num2;
+        
+        double result = (num3 / num4) * 1000.0;
+        return (int)result;
+    }
+}
