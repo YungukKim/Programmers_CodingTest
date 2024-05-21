caa79bff2d0355271b6d831deacc0b3e1ab5b0ae
Yunguk Kim / 김윤국 <160746652+YungukKim@users.noreply.github.com>
2024-05-21 21:52:29 +0900

    [level 0] Title: 각도기, Time: 0.03 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int angle) {
+        int answer = 0;
+        
+        if(angle > 0 && angle < 90){ // 예각
+            answer = 1;
+        }else if(angle == 90){ // 직각
+            answer = 2;
+        }else if(angle > 90 && angle < 180){ // 둔각
+            answer = 3;
+        }else if(angle == 180){ // 평각
+            answer = 4;
+        }
+        return answer;
+    }
+}
