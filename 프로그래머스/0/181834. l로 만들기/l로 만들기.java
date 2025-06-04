e6ed9cce27514c5c041a71741c252c8df5bdf0e0
Yunguk Kim / 김윤국 <160746652+YungukKim@users.noreply.github.com>
2025-06-05 00:19:11 +0900

    [level 0] Title: l로 만들기, Time: 10.09 ms, Memory: 100 MB -BaekjoonHub

---
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String myString) {
+        StringBuilder answer = new StringBuilder();
+
+        for (int i = 0; i < myString.length(); i++) {
+            char ch = myString.charAt(i);
+            if (ch >= 'a' && ch < 'l') {
+                answer.append('l');
+            } else {
+                answer.append(ch);
+            }
+        }
+        return answer.toString();
+    }
+}
